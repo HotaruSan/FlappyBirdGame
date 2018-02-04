3966b09de779111ea4b9e3efe96f533d6df634c2
Alexander Bespalov <alex.bespalov@hotmail.com>
2018-02-04 22:26:23 +0600

    First commit of work project FlappyBirdGame

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
+/** Спасибо огромное Ирине Галкиной https://youtu.be/r2uRVMTafEg
+ * Благодаря её видеоурокам, процесс самообучения сдвинулся с места
+ */
 //Основной класс, отвечающий за запуск нашего приложения
 public class DesktopLauncher {
 	public static void main (String[] arg) {
